b3f123c7534bc3ec3c45735b94cf891d2cea7e0e
sudz4 <mwsuther@syr.edu>
2024-12-23 19:34:25 -0800

    what

--- x_headless_screen_capture_utility/hscu.py
+++ x_headless_screen_capture_utility/hscu.py
@@ -4,9 +4,3 @@ import subprocess
 from datetime import datetime
 import pyfiglet
 
-def create_screen_capture_directory():
-    # Create a directory to store the screen captures
-    directory = "/Users/sudz4/Desktop/X/x/x_headless_screen_capture_utility/hscu_output"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    return directory
